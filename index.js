6a4d1e99d7b0777a086b95cd5129ec38c921d5c4
Ruby Weiner <rubyweiner796@me.com>
2018-08-10 13:24:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'd27a9d50c99fe976945ec2cf05a624c43afd5ca4'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
